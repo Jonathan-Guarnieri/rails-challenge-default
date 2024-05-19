f0e0608fef9d3bcdadf9ad4e002abfb95ca1388d
Jonathan Guarnieri <djou.guarnieri@hotmail.com>
2024-05-19 11:41:53 -0300

    improve users factory to avoid model validation errors

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,11 +1,11 @@
 FactoryBot.define do
   factory :user do
-    email { Faker::Internet.email }
-    phone_number { Faker::PhoneNumber.phone_number }
+    email { Faker::Internet.unique.email }
+    phone_number { Faker::PhoneNumber.unique.cell_phone_in_e164[1..20] }
     full_name { Faker::Name.name }
-    password { Faker::Internet.password(min_length: 8)}
-    key { SecureRandom.hex }
-    account_key { SecureRandom.hex }
-    metadata { Faker::Lorem.paragraph }
+    password { Faker::Internet.password(min_length: 8, max_length: 100) }
+    key { SecureRandom.hex(10) }
+    account_key { SecureRandom.hex(10) }
+    metadata { Faker::Lorem.characters(number: 2000) }
   end
 end
