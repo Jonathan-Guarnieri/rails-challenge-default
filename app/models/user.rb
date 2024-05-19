bfac91c8aacc18d1f2df079abc2e5fdf566e54f7
Jonathan Guarnieri <djou.guarnieri@hotmail.com>
2024-05-19 11:41:17 -0300

    add validations to User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,9 @@
 class User < ApplicationRecord
+  validates :email, presence: true, uniqueness: true, length: { maximum: 200 }
+  validates :phone_number, presence: true, uniqueness: true, length: { maximum: 20 }
+  validates :full_name, length: { maximum: 200 }, allow_blank: true
+  validates :password, presence: true, length: { maximum: 100 }
+  validates :key, presence: true, uniqueness: true, length: { maximum: 100 }
+  validates :account_key, uniqueness: true, length: { maximum: 100 }, allow_blank: true
+  validates :metadata, length: { maximum: 2000 }, allow_blank: true
 end
